d32d1dbf0b92017fca5ea212c2d10793237a3357
crfergus <102185709+crfergus@users.noreply.github.com>
2025-06-16 19:22:28 +0400

    analytics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -224,6 +224,23 @@ const config: EnhancedConfig = {
         content: '#FF6B00',
       },
     },
+    {
+      tagName: 'script',
+      attributes: {},
+      innerHTML: `
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+        var u="//analytics.savant.chat/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      `,
+    },
   ],
 };
 
